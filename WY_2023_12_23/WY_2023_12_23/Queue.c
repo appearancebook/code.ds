a94471b275cded9f2d720eaf8a98e36b9ab6e20e
样子书 <1144320763@qq.com>
2023-12-23 23:25:43 +0800

    学习栈和队列

---
+++ WY_2023_12_23/WY_2023_12_23/Queue.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Queue.h"
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+
+	pq->phead = NULL;
+	pq->ptail = NULL;
+	pq->size = 0;
+
+}
+
+void QueueDestroy(Queue* pq)
+{
+	assert(pq);
+	QNode* cur = pq->phead;
+	while (cur)
+	{
+		QNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	pq->phead = NULL;
+	pq->ptail = NULL;
+	pq->size = 0;
+}
+
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+	if (newnode == NULL)
+	{
+		perror("QueuePush malloc");
+		return;
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+	if (pq->phead == NULL)
+	{
+		assert(pq->ptail == NULL);
+		pq->phead = newnode;
+		pq->ptail = newnode;
+		//pq->size++;
+	}
+	else
+	{
+		pq->ptail->next = newnode;
+		pq->ptail = newnode;
+	}
+	pq->size++;
+}
+
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	QNode* next = pq->phead->next;
+	free(pq->phead);
+	pq->phead = next;
+	pq->size--;
+}
+
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	return pq->phead->data;
+}
+
+QDataType QueueBack(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	return pq->ptail->data;
+}
+
+int  QueueSize(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+
+	return pq->size;
+}
+
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+
+	//return (pq->phead == NULL
+	//	&& pq->ptail == NULL);
+
+	return pq->size == 0;
+}
---
+++ WY_2023_12_23/WY_2023_12_23/Stack.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "stack.h"
+
+void STInit(ST* pst)
+{
+	assert(pst);
+	pst->a = NULL;
+	pst->capacity = 0;
+	pst->top = 0;
+}
+
+void STDestroy(ST* pst)
+{
+	assert(pst);
+	free(pst->a);
+	pst->a = NULL;
+	pst->capacity = 0;
+	pst->top = 0;
+}
+
+void STPush(ST* pst, STDataType x)
+{
+	assert(pst);
+	if (pst->top == pst->capacity)
+	{
+		int newcapacity = pst->capacity == 0 ? 3 : pst->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(pst->a, newcapacity * sizeof(STDataType));
+		if (tmp == NULL)
+		{
+			perror("STPush realloc");
+			return;
+		}
+		pst->a = tmp;
+		pst->capacity = newcapacity;
+	}
+
+	pst->a[pst->top] = x;
+	pst->top++;
+}
+
+void STPop(ST* pst)
+{
+	assert(pst);
+	assert(!STEmpty(pst));
+
+	pst->top--;
+}
+
+STDataType STTop(ST* pst)
+{
+	assert(pst);
+	assert(!STEmpty(pst));
+
+	return pst->a[pst->top - 1];
+}
+
+bool STEmpty(ST* pst)
+{
+	assert(pst);
+
+	return pst->top == 0;
+}
+
+int STSize(ST* pst)
+{
+	assert(pst);
+
+	return pst->top;
+}
