7d7aa7734af3b9c7c2c441b286482f4d90b6d349
样子书 <1144320763@qq.com>
2024-01-02 23:55:40 +0800

    学习二叉树

---
+++ WY_2024_1_2/WY_2024_1_2/test.c
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <time.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef int BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* BuyNode(BTDataType x)
+{
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	if (node == NULL)
+	{
+		perror("BuyNode malloc");
+		return NULL;
+	}
+	node->data = x;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
+BTNode* CreatBinaryTree()
+{
+	BTNode* node1 = BuyNode(1);
+	BTNode* node2 = BuyNode(2);
+	BTNode* node3 = BuyNode(3);
+	BTNode* node4 = BuyNode(4);
+	BTNode* node5 = BuyNode(5);
+	BTNode* node6 = BuyNode(6);
+
+	node1->left = node2;
+	node1->right = node4;
+	node2->left = node3;
+	node4->left = node5;
+	node4->right = node6;
+	return node1;
+}
+
+void PrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+
+	printf("%d ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+
+	printf("%d ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+
+void Order(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+
+	printf("%d ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+
+
+// 求节点个数
+void BTreeSize(BTNode* root)
+{
+	
+}
+
+// 求叶子节点个数
+void BTreeleafSize(BTNode* root)
+{
+
+}
+
+// 求二叉树的高度
+void BTreeHeight(BTNode* root)
+{
+
+}
+
+void BTreeLeveKSize(BTNode* root, int k)
+{
+
+}
+
+int main()
+{
+	BTNode* root = CreatBinaryTree();
+	PrevOrder(root);
+
+	return 0;
+}
